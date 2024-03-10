fd31917f2815f5ac3244d9720c299b7aeeba6fd3
BuildTools <maciej.bodnar2@gmail.com>
2024-03-10 13:30:17 +0100

    increment function on button and btn class

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // document.getElementById('count-el').innerText = 4;
 
-let count = 5;
+// let count = 5;
 
-count = count + 1;
-console.log(count);
+// count = count + 1;
+// console.log(count);
+
+// function increment() {
+//   count = count + 1;
+//   document.getElementById('count-el');.innerText = count;
+//   console.log(count);
+// }
+
+let count = 0;
+let countEl = document.getElementById('count-el');
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+  save();
+}
+
+function save() {
+  console.log(count);
+}
